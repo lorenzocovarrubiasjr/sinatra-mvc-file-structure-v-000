a11da57214db129ebd7aca22aa9dbe3682ef60f6
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-11-06 01:09:29 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,8 +4,8 @@ class Dog
   @@dogs = []
   
   def initialize(name, breed, age)
-    @name = name,
-    @breed = breed,
+    @name = name
+    @breed = breed
     @age = age 
     @@dogs << self 
   end 
